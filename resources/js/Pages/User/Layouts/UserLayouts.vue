9ac4a7baa92e5de0b6fc4be0cb3b0f8eea792ca7
unknown <iamfake985@gmail.com>
2024-12-16 18:13:51 +0700

    first commit

---
+++ resources/js/Pages/User/Layouts/UserLayouts.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+import Hero from './Hero.vue';
+import NavBar from './navBar.vue';
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+
+
+
+</script>
+<template>
+    <!-- header -->
+    <NavBar></NavBar>
+    <!-- end -->
+    <!-- main content -->
+
+    <slot />
+    <!-- end -->
+    <!-- footer -->
+
+    <Footer></Footer>
+
+    <!-- end -->
+</template>
